f8fd73f60046e9420646383917246fbde701dd32
Natasha Chiu <chiuchiu07@gmail.com>
2023-06-28 05:03:53 +0800

    assert arrays equal

---
+++ assertArraysEqual.js
@@ -0,0 +1,19 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  eqArrays(arr1, arr2) ?
+    console.log("✅ The arrays are equal") :
+    console.log("🛑 The arrays are not equal");
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
