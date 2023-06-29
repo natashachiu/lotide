9dccccd88aabbd51e13ea571c2ab60c99b600f86
Natasha Chiu <chiuchiu07@gmail.com>
2023-06-30 03:05:29 +0800

    take until function

---
+++ takeUntil.js
@@ -0,0 +1,40 @@
+const takeUntil = function(array, callback) {
+  let output = [];
+
+  for (const item of array) {
+    if (!callback(item))
+      output.push(item);
+    else
+      return output;
+  }
+  return output;
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  eqArrays(actual, expected) ?
+    console.log(`✅  Assertion Passed: ${actual} === ${expected}`) :
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+assertArraysEqual(results2, ['I\'ve', 'been', 'to', 'Hollywood']);
