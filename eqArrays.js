9e903536cafcce33581f3810f402dc76aff393f3
Natasha Chiu <chiuchiu07@gmail.com>
2023-07-03 20:49:29 +0800

    eqArrays compared recursively

--- eqArrays.js
+++ eqArrays.js
@@ -8,11 +8,34 @@ const assertEqual = function(actual, expected) {
 
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) return false;
+
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) return false;
+
+    if (Array.isArray(arr1[i])) {
+      if (!eqArrays(arr1[i], arr2[i]))
+        return false;
+
+    } else if (arr1[i] !== arr2[i]) {
+      return false;
+    }
   }
   return true;
 };
+
+const arr1 = eqArrays([[2, 3], [4]], [[2, 3], [4]]); // => true
+
+const arr2 = eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]); // => false
+const arr3 = eqArrays([[2, 3], [4]], [[2, 3], 4]); // => false
+
+const arr4 = eqArrays([[2, [3]], [4]], [[2, [3]], [4]]);
+const arr5 = eqArrays([[2, [3]], [4, [5, [6]]]], [[2, [3]], [4, [5, [6]]]]);
+
+assertEqual(arr1, true);
+assertEqual(arr2, false);
+assertEqual(arr3, false);
+assertEqual(arr4, true);
+assertEqual(arr5, true);
+
 
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
